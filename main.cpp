fe060cda36b5ac21ffe0b32324aeab2c359343e5
anoosha-everest <160118595+anoosha-everest@users.noreply.github.com>
2024-02-21 09:49:53 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,11 @@ double division(int sum){
 }
 int main(){
     int a,b;
+    cin>>a>>b;
     int h;
+    cin>>h;
     int sum=addition(a,b);
     double div=division(sum);
     double area=multiplication(div,h);
+    cout<<"Trepezoid area="<<area;
 }
