aae5e76b5b0be6eaaf6c3855e5833b17f47e2675
Anoosha <anoosha.sanugula@everest.engineering>
2024-02-21 07:56:39 +0530

    adding main file

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    int h;
+    int sum=addition(a+b);
+    double div=division(sum);
+    double area=multiplication(div);
+}
