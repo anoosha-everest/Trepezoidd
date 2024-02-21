9c5086d7d3c1f87f9be3269c64b685028649639d
anoosha-everest <160118595+anoosha-everest@users.noreply.github.com>
2024-02-21 09:45:51 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 int addition(int a,int b{
     return (a+b);
-})
+}
 int main(){
     int a,b;
+    cin>>a>>b;
     int h;
+    cin>>h;
     int sum=addition(a,b);
     double div=division(sum);
     double area=multiplication(div,h);
