46d4160e24443c705367f788177ef690d7c3aced
anoosha-everest <160118595+anoosha-everest@users.noreply.github.com>
2024-02-21 09:45:14 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@ int addition(int a,int b{
 }
 int main(){
     int a,b;
+    cin>>a>>b;
     int h;
+    cin>>h;
     int sum=addition(a,b);
     double div=division(sum);
     double area=multiplication(div);
