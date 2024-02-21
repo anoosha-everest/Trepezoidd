b1f4ea564e2ac92c2592e5f684e7eb9ba43eb09d
Anoosha <anoosha.sanugula@everest.engineering>
2024-02-21 08:08:44 +0530

    adding addition logic

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
+int addition(int a,int b{
+    return (a+b);
+})
 int main(){
     int a,b;
     int h;
-    int sum=addition(a+b);
+    int sum=addition(a,b);
     double div=division(sum);
     double area=multiplication(div);
 }
